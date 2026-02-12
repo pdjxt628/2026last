5679e05db4b22180a8ffe30699d557d64e9fafea
Zaroor Mori <morza9696@gmail.com>
2026-02-12 10:15:31 +0900

    質問1のタスクを追加しましたーザーロールモーリ

---
+++ finaltest/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("値を入力してください: ");
+string input = Console.ReadLine();
+
+int number;
+bool isInteger = int.TryParse(input, out number);
+
+if (isInteger)
+{
+    Console.WriteLine("入力した値は整数です。");
+}
+else
+{
+    Console.WriteLine("入力した値は整数ではありません。");
+}
+
+Console.WriteLine("終了するには任意のキーを押してください...");
+Console.ReadKey();
