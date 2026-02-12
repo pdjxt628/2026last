6219ef9df386e380ed8b3a931aff323bd65ae98d
siddheshingale <game22403050@gmail.com>
2026-02-12 10:04:27 +0900

    整数テスト

---
+++ TatenoFinal/TatenoFinal/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Stephenです。");
+string input = Console.ReadLine();
+if(float.TryParse(input, out float result))
+{
+    if ((int)result == float.Parse(input))
+    {
+        Console.WriteLine("整数です");
+    }
+    else
+    {
+        Console.WriteLine("整数ではないです");
+    }
+}
+else
+{
+    Console.WriteLine("数字じゃないです。");
+}
