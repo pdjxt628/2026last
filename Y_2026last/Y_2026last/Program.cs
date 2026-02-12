7970150ceb4c768a0d7b334a63a5040f320b4844
YasunagaMasaaki <game22403016@gmail.com>
2026-02-12 09:46:49 +0900

    y

---
+++ Y_2026last/Y_2026last/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y_2026last
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("数値を入力してください: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int value))
+            {
+                Console.WriteLine($"整数です: {value}");
+            }
+            else
+            {
+                Console.WriteLine("整数ではありません。");
+            }
+
+            Console.WriteLine("Enterで終了します。");
+            Console.ReadLine();
+        }
+    }
+}
