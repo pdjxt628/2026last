ecf77974a7497d85b2821fdc45b819cb38436f47
ikedaharuki2 <game22403007@gmail.com>
2026-02-12 09:58:40 +0900

    a

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("数値を入力してください: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            Console.WriteLine("入力された値は整数です。");
+            Console.WriteLine("整数値: " + number);
+        }
+        else
+        {
+            Console.WriteLine("入力された値は整数ではありません。");
+        }
+    }
+}
