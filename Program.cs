8c8bdee180d379c9c6fe26f9cfca15b0f8e8486a
dennti <game22403053@gmail.com>
2026-02-16 15:49:28 +0900

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("input: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int result))
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
+
