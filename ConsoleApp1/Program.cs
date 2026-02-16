e1decd062e982d45e63cf2292c9524c5d24d5821
sokoyuu <game22403040@gmail.com>
2026-02-16 16:30:41 +0900

    11

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("入力してください：");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int result))
+        {
+            Console.WriteLine("整数です");
+        }
+        else
+        {
+            Console.WriteLine("整数ではありません");
+        }
+
+        Console.ReadLine();
+    }
+}
